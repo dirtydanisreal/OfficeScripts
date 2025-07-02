5760ee4745e67c0cb1ff3364a5017ad290aa04f4
Max Halcomb <iambosh93@gmail.com>
2025-07-02 07:47:06 -0400

    Update and rename AddName.osts to AddName.ts

--- AddName.ts
+++ AddName.ts
@@ -2,12 +2,18 @@ function main(workbook: ExcelScript.Workbook) {
   let sheet = workbook.getActiveWorksheet();
   const protection = sheet.getProtection();
 
-  // Temporarily pause protection with the password
-  protection.pauseProtection("imsorrydave");
+  if (sheet.getName() === "PHONE 7.200 ICU") {
+    protection.pauseProtection("72icu");
+  }
+  else{
+    protection.pauseProtection("imsorrydave");
+  }
 
   // Define the necessary sheets and ranges
   let listSheet = workbook.getWorksheet("RN-NCT LIST");
   let psSheet = workbook.getWorksheet("PHONE 6.100 AC-PC") || workbook.getWorksheet("PHONE 6.200 AC-PC") || workbook.getWorksheet("PHONE 6.200 ICU") || workbook.getWorksheet("PHONE 7.200 ICU") || workbook.getWorksheet("PHONE 7.100 AC-PC");
+
+
   let numbersRange = listSheet.getRange("A1:D900");
   let values = numbersRange.getValues();
 
@@ -19,12 +25,24 @@ function main(workbook: ExcelScript.Workbook) {
   const last = psSheet.getRange("L4").getValue().toString().toUpperCase().trim();
   const num = psSheet.getRange("L5").getValue().toString().toUpperCase().trim();
 
+ if(psSheet.getName() == "PHONE 6.200 ICU" || "PHONE 7.200 ICU")
+  {
+    const first = psSheet.getRange("Q5").getValue().toString().toUpperCase().trim();
+    const last = psSheet.getRange("Q6").getValue().toString().toUpperCase().trim();
+    const num = psSheet.getRange("Q7").getValue().toString().toUpperCase().trim();
+  }
+
   // Insert values into RN-NCT LIST sheet at the next available row
   // Note: Using getRangeByIndexes() to set a range with multiple columns
   listSheet.getRangeByIndexes(lastUsedRow, 0, 1, 4).setValues([[first, last, null, num]]);
 
   // Remove duplicates from columns A, B, and D
   numbersRange.removeDuplicates([0, 1, 3], false);
+
+  if(psSheet.getName() === "PHONE 7.200 ICU" || "PHONE 6.200 ICU")
+  {
+    psSheet.getRange("Q5:Q7").clear(ExcelScript.ClearApplyTo.contents);
+  }
 
   // Clear contents of L3, L4, and L5 in PHONE 6.100 AC-PC sheet
   psSheet.getRange("L3:L5").clear(ExcelScript.ClearApplyTo.contents);
